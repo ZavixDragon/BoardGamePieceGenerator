fdaaa34bfc2b354cf990f63dab86b9ef1c68aa66
noah reinagel <noah.r9@gmail.com>
2018-09-13 04:04:30 -0700

    Made progress deciphering project

--- Generator/Generator/Program.cs
+++ Generator/Generator/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Generator
 {
@@ -6,7 +10,16 @@ namespace Generator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var templatePath = args[0];
+            var listPath = args[1];
+            var templateJson = File.ReadAllText(templatePath);
+            var listJson = File.ReadAllText(templatePath);
+            var template = JObject.Parse(templateJson);
+            var list = JArray.Parse(listJson);
+            foreach (var item in list.Children<JObject>())
+            {
+                var bitmap = new Bitmap
+            }
         }
     }
 }
